07b9d42cf59b6e26dbf18b47b42c554e61ed9ff2
Nicholas <nicpayne713@gmail.com>
2021-09-07 21:31:48 -0500

    Add ease shell scripts

---
+++ encrypt-stuff.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ansible-vault encrypt inventories/my-ansible-nas/inventory.yml --vault-password-file key
+ansible-vault encrypt inventories/my-ansible-nas/group_vars/nas.yml --vault-password-file key
+ansible-vault encrypt inventories/my-ansible-nas/group_vars/sandbox.yml --vault-password-file key
