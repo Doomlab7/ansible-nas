1b1cf85e610de5cd6c96be60cf2c2edaf0c9bec9
pypeaday <pypeaday@protonmail.com>
2022-11-12 06:58:09 -0600

    wip
    WIP: add defaults for kvm-bridge
    
    pc
    
    encrypt ducky
    
    add ducky vars
    
    add playbook for ducky - it's the same just with hosts: ducky
    
    updates
    
    update url for sd

--- encrypt-stuff.sh
+++ encrypt-stuff.sh
@@ -2,3 +2,4 @@
 ansible-vault encrypt inventories/my-ansible-nas/inventory.yml --vault-password-file key
 ansible-vault encrypt inventories/my-ansible-nas/group_vars/nas.yml --vault-password-file key
 ansible-vault encrypt inventories/my-ansible-nas/group_vars/sandbox.yml --vault-password-file key
+ansible-vault encrypt inventories/my-ansible-nas/group_vars/ducky.yml --vault-password-file key
